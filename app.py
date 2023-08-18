11aa6da8449467e61bab8306079caaae0f627c95
NoemiHuarino-utec <91573449+NoemiHuarino-utec@users.noreply.github.com>
2023-08-17 20:32:21 -0500

    New change, multiplicacion

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 
 def hello():
     return '<h1>Hello, Universe, esto es Feature1!</h1>'
+
+def multiplicar(param1, param2):
+    return param1 * param2
+
+param1 = 10
+param2 = 20
+resultado = multiplicar(param1, param2)
+print("La multiplicación de 10 y 20 es:", resultado)
+
